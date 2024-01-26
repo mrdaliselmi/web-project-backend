1ec478f38dd58562d18eb395b239d64f8dba2e90
MohamedAliSelmi <mohamedali.selmi@insat.u-carthage.tn>
2024-01-27 00:16:50 +0100

    🔧 chore: enable cors & setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        database: config.get('DB_DATABASE'),
+        host: config.get('DB_HOST'),
+        username: config.get('DB_USERNAME'),
+        port: config.get('DB_PORT'),
+        password: config.get('DB_PASSWORD'),
+        entities: [],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
